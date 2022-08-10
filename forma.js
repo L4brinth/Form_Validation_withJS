93adc2af46b77f75e601e25c59211ebbac3455c8
Emil Jelavic <emiljelavic@Emils-MacBook-Pro.local>
2022-08-10 16:19:46 +0200

    updated js

--- forma.js
+++ forma.js
@@ -17,7 +17,12 @@ const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const phoneRegex = /\+[0-9]{10}$/;
 
 // FORM VALIDATION for EMAIL and PHONE
+
+
 const validationEmailPhone = (field, regex, error1, error2) => {
+
+    // field.nextElementSibling.innerHTML = field.value === "" ? error1 : !field.value.match(regex) ? error2 : "";
+
     if (field.value === "") {
         field.nextElementSibling.innerHTML = error1;
         return false;
@@ -32,6 +37,9 @@ const validationEmailPhone = (field, regex, error1, error2) => {
 
 //FORM VALIDATION for FIRSTNAME, LASTNAME and ADDRESS
 const validationNameAddress = (field, error) => {
+
+    // field.nextElementSibling.innerHTML = field.value === "" ? error : field.nextElementSibling.innerHTML = "";
+
     if (field.value === "") {
         field.nextElementSibling.innerHTML = error;
         return false;
@@ -43,6 +51,9 @@ const validationNameAddress = (field, error) => {
 
 // VALIDATION for CHECKBOX
 const validationCheckbox = () => {
+
+    // checkboxEl.nextElementSibling.innerHTML = checkboxEl.checked ? "" : "Checkbox is REQUIRED.";
+
     if (checkboxEl.checked) {
         checkboxEl.nextElementSibling.innerHTML = "";
         return true;
@@ -53,23 +64,33 @@ const validationCheckbox = () => {
 };
 
 const validateForm = (e) => {
+
     !validationNameAddress(firstNameEl, emptyInput) && e.preventDefault();
     !validationNameAddress(lastNameEl, emptyInput) && e.preventDefault();
     !validationNameAddress(addressEl, emptyInput) && e.preventDefault();
     !validationEmailPhone(emailEl, emailRegex, emptyInput, emailInvalid) && e.preventDefault();
     !validationEmailPhone(phoneEl, phoneRegex, emptyInput, phoneInvalid) && e.preventDefault();
     !validationCheckbox() && e.preventDefault();
+
+    // e.preventDefault();
+    // !validationNameAddress(firstNameEl, emptyInput);
+    // !validationNameAddress(lastNameEl, emptyInput);
+    // !validationNameAddress(addressEl, emptyInput);
+    // !validationEmailPhone(emailEl, emailRegex, emptyInput, emailInvalid);
+    // !validationEmailPhone(phoneEl, phoneRegex, emptyInput, phoneInvalid);
+    // !validationCheckbox();
+
 }
 
 // On submit show all errors (if any)
 formEl.addEventListener("submit", validateForm);
 
 // On blur SHOW errors 
-emailEl.addEventListener("blur", (e) => validationEmailPhone(emailEl, emailRegex, emptyInput, emailInvalid, e));
-firstNameEl.addEventListener("blur", (e) => validationNameAddress(firstNameEl, emptyInput, e));
-lastNameEl.addEventListener("blur", (e) => validationNameAddress(lastNameEl, emptyInput, e));
-addressEl.addEventListener("blur", (e) => validationNameAddress(addressEl, emptyInput, e));
-phoneEl.addEventListener("blur", (e) => validationEmailPhone(phoneEl, phoneRegex, emptyInput, phoneInvalid, e));
+emailEl.addEventListener("blur", () => validationEmailPhone(emailEl, emailRegex, emptyInput, emailInvalid));
+firstNameEl.addEventListener("blur", () => validationNameAddress(firstNameEl, emptyInput));
+lastNameEl.addEventListener("blur", () => validationNameAddress(lastNameEl, emptyInput));
+addressEl.addEventListener("blur", () => validationNameAddress(addressEl, emptyInput));
+phoneEl.addEventListener("blur", () => validationEmailPhone(phoneEl, phoneRegex, emptyInput, phoneInvalid));
 
 // On focus HIDE errors
 emailEl.addEventListener("focus", () => emailEl.nextElementSibling.innerHTML = "");
